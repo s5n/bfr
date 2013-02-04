87cc5da8bcd10e48bfa645af28b672504ab462b1
Stephan Leinweber <pseudo@informatik.uni-bremen.de>
2013-02-04 08:51:06 +0100

    Erster Test mit Abrufen der Links auf bsag.de

--- bfr.rb
+++ bfr.rb
@@ -1,15 +1,38 @@
 # "mabf" reloaded
 # Kleines Skript um mal eben zu sehen welche Bahn/Bahn
 # in den nächsten Minuten abfährt. 
-
 require 'rubygems'
 require 'nokogiri'
 require 'mechanize'
 
-agent = Mechanize.new
+#source, destination, 
+# Univers./Zentralbereich
+# Hauptbahnhof
 
-page = agent.get('http://google.com/')
+class Bsag
+  def initialize(start_station="Univers./Zentralbereich", stop_station="Hauptbahnhof")
+    @start_station = start_station
+    @stop_station = stop_station
+  end
 
-page.links.each do |link|
-  puts link.text
+  def search_connection
+    agent = Mechanize.new { |a| a.user_agent = ' Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:18.0) Gecko/20100101 Firefox/18.0' }
+    page = agent.get('http://www.bsag.de/')
+    
+    page.links.each do |link|
+      puts link.text
+    end
+
+    puts "nun noch die Haltestellen"
+    puts self.start_station
+    puts self.stop_station
+  end
+  
+  
 end
+
+bsag = Bsag.new(ARGV[0], ARGV[1])
+bsag.search_connection
+
+
+
